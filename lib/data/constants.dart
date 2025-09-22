58049f829e58059ed09ca6473db1bb5d0900077c
nguyenbinh_turbo <binhbn@lotusys.vn>
2025-09-22 18:22:08 +0700

    add2

---
+++ lib/data/constants.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+class KTextStyle {
+  static const TextStyle titleTealText = TextStyle(color: Colors.lightBlue, fontSize: 18.0, fontWeight: FontWeight.bold, letterSpacing: 10);
+  static const TextStyle descriptionText = TextStyle(fontSize: 16.0);
+}
